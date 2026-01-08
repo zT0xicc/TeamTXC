aa4fdf30ac11e72c7f7138a81bd0f5c7edfbab0f
zT0xicc <elijahvm@icloud.com>
2026-01-08 00:55:36 -0500

    Bug fixes

--- js/main.js
+++ js/main.js
@@ -11,13 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!textElement) return;
 
   setInterval(() => {
+    // Fade out
     textElement.classList.add("fade-out");
 
     setTimeout(() => {
+      // Change text
       index = (index + 1) % taglines.length;
       textElement.textContent = taglines[index];
-      textElement.classList.remove("fade-out");
+
+      textElement.classList.remove("fade-out", "fade-in");
+
+      void textElement.offsetWidth;
+
       textElement.classList.add("fade-in");
     }, 400);
+
   }, 3000);
 });
